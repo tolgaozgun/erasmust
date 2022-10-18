604e18fe8b26c052cff360e0ad669990ec5cba4a
BarisYildirim6 <byildirim2561@gmail.com>
2022-10-18 18:14:27 +0300

    Second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Tolgayı tten skm.
+          Selamlar canlarım ...
         </p>
         <a
           className="App-link"
