d834c94a9170f02d0a44b4dc10430dc1176ed2f2
Yeahya35 <yahyaeren35@hotmail.com>
2022-10-18 18:42:16 +0300

    log added
    check if server is running

--- backend/src/main/java/com/bilkent/erasmus/ErasmusApplication.java
+++ backend/src/main/java/com/bilkent/erasmus/ErasmusApplication.java
@@ -1,16 +1,18 @@
 package com.bilkent.erasmus;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @SpringBootApplication
 @RestController // put here to test if app is connected to the web
 public class ErasmusApplication {
 
-    @GetMapping("/message") // you can follow this url to check if localhost is running (http://127.0.0.1:8080/message)
+    @GetMapping("") // you can follow this url to check if localhost is running (http://127.0.0.1:8080/message)
     public String getMessage() {
+        log.info("cükünü yerim");
         return "stable";
     }
     public static void main(String[] args) {
