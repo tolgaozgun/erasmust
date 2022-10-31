c749e89ca8b0316de154f1f465357ea4ec0bb22c
BarisYildirim6 <byildirim2561@gmail.com>
2022-10-31 20:31:10 +0300

    Check1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Selamlar canlarım ...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="outlined">Hello World!</Button>
     </div>
   );
 }
