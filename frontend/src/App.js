34985421578fddb349b707ed1e2a392ca8a6169d
Tolga <tolgaozgunn@gmail.com>
2022-12-06 10:16:03 +0000

    Changes in React routing, added Preapproval page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,23 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import React from 'react';
-//Student Pages
-import RegisterStudent from "./pages/student/register";
-import LoginStudent from "./pages/student/login";
-import DashboardStudent from "./pages/student/dashboard";
-import AccountStudent from './pages/student/account';
-import SettingsStudent from './pages/student/settings';
-import ErasmusStudent from './pages/student/erasmus';
 
-//Stuff Pages
-import RegisterStaff from "./pages/academic/register";
-import LoginStaff from "./pages/academic/login";
-import DashboardStaff from "./pages/academic/dashboard";
-import AccountStaff from "./pages/academic/account";
-import SettingsStaff from "./pages/academic/settings";
-import ErasmusStaff from "./pages/academic/erasmus"
+import {routeItems} from "./routeConfig.tsx";
 
-import Splash from "./pages/splash";
-import Error from "./pages/404";
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/registerStudent' element={<RegisterStudent/>}>
-        </Route>
-        <Route path='/loginStudent' element={<LoginStudent/>}>
-        </Route>
-        <Route path='/dashboardStudent' element={<DashboardStudent/>}>
-        </Route>
-        <Route path='/accountStudent' element={<AccountStudent/>}>
-        </Route>
-        <Route path='/settingStudent' element={<SettingsStudent/>}>
-        </Route>
-        <Route path='/erasmusStudent' element={<ErasmusStudent/>}>
-        </Route>
+        {routeItems.map(routeItem => (
+          <Route
+            key={routeItem.title}
+            path={routeItem.path}
+            exact={routeItem.exact}
+            element={routeItem.content}
+            />
+        ))}
 
-
-        <Route path='/registerStaff' element={<RegisterStaff/>}>
-        </Route>
-        <Route path='/loginStaff' element={<LoginStaff/>}>
-        </Route>
-        <Route path='/dashboardStaff' element={<DashboardStaff/>}>
-        </Route>
-        <Route path='/accountStaff' element={<AccountStaff/>}>
-        </Route>
-        <Route path='/settingStaff' element={<SettingsStaff/>}>
-        </Route>
-        <Route path='/erasmusStaff' element={<ErasmusStaff/>}>
-        </Route>
-
-        <Route path='*' element={<Error/>}>
-        </Route>
-        <Route path='/splash' element={<Splash/>}>
-        </Route>
-
-
-        <Route exact path='/' element={<Navigate to='/splash'/>}>
-        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/routeConfig.tsx
@@ -0,0 +1,125 @@
+import {IRoute} from "./IRoute";
+import * as React from "react";
+
+//Student Pages
+import RegisterStudent from "./pages/student/register";
+import LoginStudent from "./pages/student/login";
+import DashboardStudent from "./pages/student/dashboard";
+import AccountStudent from './pages/student/account';
+import SettingsStudent from './pages/student/settings';
+import ErasmusStudent from './pages/student/erasmus';
+import Preapproval from './pages/student/preapproval';
+
+//Staff Pages
+import RegisterStaff from "./pages/academic/register";
+import LoginStaff from "./pages/academic/login";
+import DashboardStaff from "./pages/academic/dashboard";
+import AccountStaff from "./pages/academic/account";
+import SettingsStaff from "./pages/academic/settings";
+import ErasmusStaff from "./pages/academic/erasmus"
+
+import Splash from "./pages/splash";
+import Error from "./pages/404";
+
+import {Navigate} from "react-router-dom";
+
+export const routeItems: Array<IRoute> = [
+    {
+        path: '/registerStudent',
+        title: "Student Register",
+        content: <RegisterStudent />,
+        exact: true,
+    },
+    {
+        path: '/loginStudent',
+        title: "Student Register",
+        content: <LoginStudent/>,
+        exact: true,
+    },
+    {
+        path: '/dashboardStudent',
+        title: "Student Register",
+        content: <DashboardStudent/>,
+        exact: true,
+    },
+    {
+        path: '/accountStudent',
+        title: "Student Register",
+        content: <AccountStudent/>,
+        exact: true,
+    },
+    {
+        path: '/settingsStudent',
+        title: "Student Register",
+        content: <SettingsStudent/>,
+        exact: true,
+    },
+    {
+        path: '/erasmusStudent',
+        title: "Student Register",
+        content: <ErasmusStudent/>,
+        exact: true,
+    },
+    {
+        path: '/registerStaff',
+        title: "Student Register",
+        content: <RegisterStaff/>,
+        exact: true,
+    },
+    {
+        path: '/loginStaff',
+        title: "Student Register",
+        content: <LoginStaff/>,
+        exact: true,
+    },
+    {
+        path: '/dashboardStaff',
+        title: "Student Register",
+        content: <DashboardStaff/>,
+        exact: true,
+    },
+        {
+            path: '/accountStaff',
+            title: "Student Register",
+            content: <AccountStaff/>,
+            exact: true,
+        },
+        {
+            path: '/settingsStaff',
+            title: "Student Register",
+            content: <SettingsStaff/>,
+            exact: true,
+        },
+        {
+            path: '/erasmusStaff',
+            title: "Student Register",
+            content: <ErasmusStaff/>,
+            exact: true,
+        },
+        {
+            path: '/preapproval',
+            title: "Student Register",
+            content: <Preapproval/>,
+            exact: true,
+        },
+        {
+            path: '*',
+            title: "Student Register",
+            content: <Error/>,
+            exact: false,
+        },
+        {
+            path: '/splash',
+            title: "Student Register",
+            content: <Splash/>,
+            exact: true,
+        },
+        {
+            path: '/',
+            title: "Student Register",
+            content: <Navigate to='/splash' />,
+            exact: true,
+        }
+]
+
+
