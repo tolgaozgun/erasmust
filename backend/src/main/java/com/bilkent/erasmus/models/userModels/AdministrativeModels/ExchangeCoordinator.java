c5e5e98dc8149eebedb2efab4bb1cd799d161b97
Yeahya35 <yahyaeren35@hotmail.com>
2022-12-06 14:03:25 +0300

    mapping conflict resolved
    exchange coordinator extends course coordinators

--- backend/src/main/java/com/bilkent/erasmus/models/userModels/AdministrativeModels/ExchangeCoordinator.java
+++ backend/src/main/java/com/bilkent/erasmus/models/userModels/AdministrativeModels/ExchangeCoordinator.java
@@ -12,10 +12,7 @@ import javax.persistence.Table;
 @Data
 @Entity
 @Table(name = "exchangeCoordinators")
-public class ExchangeCoordinator extends Administrative { // its names is replaced, previous name: Coordinator
-
-    @ManyToOne
-    private Department department;
+public class ExchangeCoordinator extends CourseCoordinator { // its names is replaced, previous name: Coordinator
 
     // @workLoad will be expanded in the future to balance the paper
     // work between coordinators. For the sake of simplicity
