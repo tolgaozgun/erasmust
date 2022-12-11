9d463505f757c7332c4982103f75d0969ecc04b1
ebuyukkonuklu <buyukkonukluemirhan@gmail.com>
2022-12-11 23:05:28 +0300

    ToDoList(not finished yet)

---
+++ frontend/src/components/componentsStaff/TodoComponent/ToDo.js
@@ -0,0 +1,76 @@
+import React, { Component, Fragment } from "react";
+import { Delete, Build } from "@material-ui/icons";
+import { Grid, Paper } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import { CSSTransition } from "react-transition-group";
+
+const styles = {
+    Icon: {
+        marginLeft: "auto",
+    },
+    Paper: {
+        margin: "auto",
+        padding: 10,
+        display: "flex",
+        alignItems: "center",
+        marginTop: 10,
+        width: 500,
+    },
+};
+
+class ToDo extends Component {
+    state = {
+        fade: false,
+    };
+
+    gridRef = React.createRef();
+
+    deleteTodo = () => {
+        const fade = true;
+        this.setState({ fade });
+
+        var promise = new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve(true);
+            }, 500);
+        });
+
+        promise.then(() => this.props.deleteTodo(this.props.index));
+        console.log(this.state);
+    };
+
+    render() {
+        const gridClass = this.state.fade ? "fade-out" : "";
+
+        return (
+            <Grid
+                xs={12}
+                className={`${gridClass}`}
+                item
+                key={this.props.index}
+                ref={this.gridRef}
+            >
+                <Paper elevation={2} style={styles.Paper}>
+                    <span style={styles.Todo}>{this.props.todo}</span>
+                    <IconButton
+                        color="primary"
+                        aria-label="Edit"
+                        style={styles.Icon}
+                        onClick={() => this.props.updateTodo(this.props.index)}
+                    >
+                        <Build fontSize="small" />
+                    </IconButton>
+                    <IconButton
+                        color="secondary"
+                        aria-label="Delete"
+                        onClick={this.deleteTodo}
+                    >
+                        <Delete fontSize="small" />
+                    </IconButton>
+                </Paper>
+            </Grid>
+        );
+    }
+}
+
+export default ToDo;
--- frontend/src/pages/academic/dashboard.js
+++ frontend/src/pages/academic/dashboard.js
@@ -1,76 +1,59 @@
-import { Box, Container, Grid } from '@mui/material';
-import { LatestOrders } from '../../components/componentsStaff/dashboard/latest-orders';
-import { Sales } from '../../components/componentsStaff/dashboard/sales';
-import { DashboardNavbar } from '../../components/componentsStaff/dashboard-navbar';
-import { DashboardSidebar } from '../../components/componentsStaff/dashboard-sidebar';
-import React, { useState } from 'react';
-import { styled } from '@mui/material/styles';
+import { Box, Container, Grid } from "@mui/material";
+import { LatestOrders } from "../../components/componentsStaff/dashboard/latest-orders";
+import { Sales } from "../../components/componentsStaff/dashboard/sales";
+import { DashboardNavbar } from "../../components/componentsStaff/dashboard-navbar";
+import { DashboardSidebar } from "../../components/componentsStaff/dashboard-sidebar";
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import { toDoComp } from "../../components/componentsStaff/TodoComponent/ToDoComp";
 
-const DashboardLayoutRoot = styled('div')(({ theme }) => ({
-  display: 'flex',
-  flex: '1 1 auto',
-  maxWidth: '100%',
-  paddingTop: 64,
-  [theme.breakpoints.up('lg')]: {
-    paddingLeft: 280
-  }
+const DashboardLayoutRoot = styled("div")(({ theme }) => ({
+    display: "flex",
+    flex: "1 1 auto",
+    maxWidth: "100%",
+    paddingTop: 64,
+    [theme.breakpoints.up("lg")]: {
+        paddingLeft: 280,
+    },
 }));
 
 const DashboardStudent = (props) => {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
+    const [isSidebarOpen, setSidebarOpen] = useState(true);
 
-
-  return(
-  <>
-    
-      <title>
-        Dashboard | Material Kit
-      </title>
-    <DashboardLayoutRoot>
-    <Box
-      component="main"
-      sx={{
-        display: 'flex',
-        flex: '1 1 auto',
-        flexDirection: 'column',
-        width: '100%',
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <Sales />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <LatestOrders />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-    </DashboardLayoutRoot>
-    <DashboardNavbar onSidebarOpen={() => setSidebarOpen(true)} />
-        <DashboardSidebar 
-          onClose={() => setSidebarOpen(false)}
-          open={isSidebarOpen}/>
-  </>
-  );
+    return (
+        <>
+            <title>Dashboard | Material Kit</title>
+            <DashboardLayoutRoot>
+                <Box
+                    component="main"
+                    sx={{
+                        display: "flex",
+                        flex: "1 1 auto",
+                        flexDirection: "column",
+                        width: "100%",
+                        flexGrow: 1,
+                        py: 8,
+                    }}
+                >
+                    <Container maxWidth={false}>
+                        <Grid container spacing={3}>
+                            <Grid item lg={4} md={6} xl={3} xs={12}>
+                                <Sales />
+                            </Grid>
+                            <Grid item lg={8} md={12} xl={9} xs={12}>
+                                <LatestOrders />
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </DashboardLayoutRoot>
+            <DashboardNavbar onSidebarOpen={() => setSidebarOpen(true)} />
+            <DashboardSidebar
+                onClose={() => setSidebarOpen(false)}
+                open={isSidebarOpen}
+            />
+        </>
+    );
 };
 
 export default DashboardStudent;
