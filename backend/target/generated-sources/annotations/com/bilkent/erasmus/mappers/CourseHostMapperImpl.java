d47e52514978f9a6ba8bb8f5ab2a7f3ce07e31b4
Yeahya35 <yahyaeren35@hotmail.com>
2022-12-11 18:15:58 +0300

    Merge branch 'main' of C:\Users\yahya\Desktop\cs319\erasmust with conflicts.

---
+++ backend/target/generated-sources/annotations/com/bilkent/erasmus/mappers/CourseHostMapperImpl.java
@@ -0,0 +1,83 @@
+package com.bilkent.erasmus.mappers;
+
+import com.bilkent.erasmus.dtos.CourseHostDTO;
+import com.bilkent.erasmus.models.courseModels.CourseHost;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-12-07T20:35:50+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.7 (Amazon.com Inc.)"
+)
+@Component
+public class CourseHostMapperImpl implements CourseHostMapper {
+
+    @Override
+    public CourseHostDTO toCourseHostDTO(CourseHost courseHost) {
+        if ( courseHost == null ) {
+            return null;
+        }
+
+        CourseHostDTO courseHostDTO = new CourseHostDTO();
+
+        courseHostDTO.setId( courseHost.getId() );
+        courseHostDTO.setName( courseHost.getName() );
+        courseHostDTO.setCreditECTS( courseHost.getCreditECTS() );
+        courseHostDTO.setCreditBilkent( courseHost.getCreditBilkent() );
+        courseHostDTO.setIsProjectCourse( courseHost.getIsProjectCourse() );
+        courseHostDTO.setIsNeedToBeMerged( courseHost.getIsNeedToBeMerged() );
+        courseHostDTO.setIsApprovedPrev( courseHost.getIsApprovedPrev() );
+
+        return courseHostDTO;
+    }
+
+    @Override
+    public CourseHost toEntity(CourseHostDTO courseHostDTO) {
+        if ( courseHostDTO == null ) {
+            return null;
+        }
+
+        CourseHost courseHost = new CourseHost();
+
+        courseHost.setId( courseHostDTO.getId() );
+        courseHost.setName( courseHostDTO.getName() );
+        courseHost.setCreditECTS( courseHostDTO.getCreditECTS() );
+        courseHost.setCreditBilkent( courseHostDTO.getCreditBilkent() );
+        courseHost.setIsProjectCourse( courseHostDTO.getIsProjectCourse() );
+        courseHost.setIsNeedToBeMerged( courseHostDTO.getIsNeedToBeMerged() );
+        courseHost.setIsApprovedPrev( courseHostDTO.getIsApprovedPrev() );
+
+        return courseHost;
+    }
+
+    @Override
+    public List<CourseHostDTO> toCourseHostDTOList(List<CourseHost> courseHostList) {
+        if ( courseHostList == null ) {
+            return null;
+        }
+
+        List<CourseHostDTO> list = new ArrayList<CourseHostDTO>( courseHostList.size() );
+        for ( CourseHost courseHost : courseHostList ) {
+            list.add( toCourseHostDTO( courseHost ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<CourseHost> toCourseHostList(List<CourseHostDTO> courseHostDTOList) {
+        if ( courseHostDTOList == null ) {
+            return null;
+        }
+
+        List<CourseHost> list = new ArrayList<CourseHost>( courseHostDTOList.size() );
+        for ( CourseHostDTO courseHostDTO : courseHostDTOList ) {
+            list.add( toEntity( courseHostDTO ) );
+        }
+
+        return list;
+    }
+}
---
+++ backend/target/generated-sources/annotations/com/bilkent/erasmus/mappers/InitialApplicationMappper/ApplicationErasmusMapperImpl.java
@@ -0,0 +1,79 @@
+package com.bilkent.erasmus.mappers.InitialApplicationMappper;
+
+import com.bilkent.erasmus.dtos.InitialApplicationDTO.ApplicationErasmusDTO;
+import com.bilkent.erasmus.models.applicationModels.InitialApplicationModels.ApplicationErasmus;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-12-07T20:35:50+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.7 (Amazon.com Inc.)"
+)
+@Component
+public class ApplicationErasmusMapperImpl implements ApplicationErasmusMapper {
+
+    @Override
+    public ApplicationErasmusDTO toApplicationErasmusDTO(ApplicationErasmus applicationErasmus) {
+        if ( applicationErasmus == null ) {
+            return null;
+        }
+
+        ApplicationErasmusDTO applicationErasmusDTO = new ApplicationErasmusDTO();
+
+        applicationErasmusDTO.setId( applicationErasmus.getId() );
+        applicationErasmusDTO.setAppliedSemester( applicationErasmus.getAppliedSemester() );
+        applicationErasmusDTO.setSignature( applicationErasmus.getSignature() );
+        applicationErasmusDTO.setSignAt( applicationErasmus.getSignAt() );
+        applicationErasmusDTO.setCoordinator( applicationErasmus.getCoordinator() );
+
+        return applicationErasmusDTO;
+    }
+
+    @Override
+    public ApplicationErasmus toEntity(ApplicationErasmusDTO applicationErasmusDTO) {
+        if ( applicationErasmusDTO == null ) {
+            return null;
+        }
+
+        ApplicationErasmus applicationErasmus = new ApplicationErasmus();
+
+        applicationErasmus.setId( applicationErasmusDTO.getId() );
+        applicationErasmus.setAppliedSemester( applicationErasmusDTO.getAppliedSemester() );
+        applicationErasmus.setSignature( applicationErasmusDTO.getSignature() );
+        applicationErasmus.setSignAt( applicationErasmusDTO.getSignAt() );
+        applicationErasmus.setCoordinator( applicationErasmusDTO.getCoordinator() );
+
+        return applicationErasmus;
+    }
+
+    @Override
+    public List<ApplicationErasmusDTO> toApplicationErasmusDTOList(List<ApplicationErasmus> applicationErasmusList) {
+        if ( applicationErasmusList == null ) {
+            return null;
+        }
+
+        List<ApplicationErasmusDTO> list = new ArrayList<ApplicationErasmusDTO>( applicationErasmusList.size() );
+        for ( ApplicationErasmus applicationErasmus : applicationErasmusList ) {
+            list.add( toApplicationErasmusDTO( applicationErasmus ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<ApplicationErasmus> toApplicationErasmusList(List<ApplicationErasmusDTO> applicationErasmusDTOList) {
+        if ( applicationErasmusDTOList == null ) {
+            return null;
+        }
+
+        List<ApplicationErasmus> list = new ArrayList<ApplicationErasmus>( applicationErasmusDTOList.size() );
+        for ( ApplicationErasmusDTO applicationErasmusDTO : applicationErasmusDTOList ) {
+            list.add( toEntity( applicationErasmusDTO ) );
+        }
+
+        return list;
+    }
+}
