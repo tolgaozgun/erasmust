9a440cf713e5ba93838f0820edbfebb5df7546cd
BarisYildirim6 <byildirim2561@gmail.com>
2022-12-14 00:31:32 +0300

    Admin Panels added (dashboard && account)

---
+++ frontend/src/components/componentsAdmin/account/account-profile-details.js
@@ -0,0 +1,153 @@
+import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  Grid,
+  TextField
+} from '@mui/material';
+import React from 'react';
+
+export const AccountProfileDetails = (props) => {
+  const [values, setValues] = useState({
+    firstName: 'Tolga',
+    lastName: 'Özgün',
+    email: 'tolga.ozgun@ug.bilkent.edu.tr',
+    department: 'Computer Engineering',
+    starsId: 22003850,
+    id: 1,
+  });
+
+  // TODO: Change this to match current login details
+  const isAdmin = false;
+
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <form
+      autoComplete="off"
+      noValidate
+      {...props}
+    >
+      <Card>
+        <CardHeader
+            subheader="General user information"
+            title="Profile"
+        />
+        <Divider />
+        <CardContent>
+          <Grid
+              container
+              spacing={3}
+          >
+            <Grid
+                item
+                md={6}
+                xs={12}
+            >
+              <TextField
+                  fullWidth
+                  label="Stars ID"
+                  name="starsId"
+                  onChange={handleChange}
+                  required
+                  type="number"
+                  value={values.starsId}
+                  variant="outlined"
+                  disabled={!isAdmin}
+              />
+            </Grid>
+            <Grid
+                item
+                md={6}
+                xs={12}
+            >
+              <TextField
+                  fullWidth
+                  label="Account ID"
+                  name="accountId"
+                  onChange={handleChange}
+                  type="number"
+                  required
+                  value={values.id}
+                  variant="outlined"
+                  disabled={!isAdmin}
+              />
+            </Grid>
+            <Grid
+                item
+                md={6}
+                xs={12}
+            >
+              <TextField
+                  fullWidth
+                  // helperText="Please specify the first name"
+                  label="First name"
+                  name="firstName"
+                onChange={handleChange}
+                required
+                value={values.firstName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Last name"
+                name="lastName"
+                onChange={handleChange}
+                required
+                value={values.lastName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Email Address"
+                name="email"
+                onChange={handleChange}
+                required
+                value={values.email}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                  fullWidth
+                  label="Department"
+                  name="department"
+                  onChange={handleChange}
+                  value={values.department}
+                  variant="outlined"
+                  required
+                  disabled={!isAdmin}
+              />
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </form>
+  );
+};
---
+++ frontend/src/components/componentsAdmin/dashboard/latest-orders.js
@@ -0,0 +1,135 @@
+import { format } from 'date-fns';
+import { v4 as uuid } from 'uuid';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  Tooltip
+} from '@mui/material';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { SeverityPill } from '../severity-pill';
+import React from 'react';
+
+const orders = [
+  {
+    id: uuid(),
+    ref: 'CDD1049',
+    amount: 30.5,
+    customer: {
+      name: 'Erasmus Application'
+    },
+    createdAt: 1555016400000,
+    status: 'approved'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1048',
+    amount: 25.1,
+    customer: {
+      name: 'Preapproval Form'
+    },
+    createdAt: 1555016400000,
+    status: 'approved'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1047',
+    amount: 10.99,
+    customer: {
+      name: 'Learning Agreement'
+    },
+    createdAt: 1554930000000,
+    status: 'pending'
+  }
+];
+
+export const LatestOrders = (props) => (
+  <Card {...props}>
+    <CardHeader title="Ongoing Processes" />
+    <PerfectScrollbar>
+      <Box sx={{ 
+        minWidth: 800,
+        height: 400 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                Process ID
+              </TableCell>
+              <TableCell>
+                Process Name
+              </TableCell>
+              <TableCell sortDirection="desc">
+                <Tooltip
+                  enterDelay={300}
+                  title="Sort"
+                >
+                  <TableSortLabel
+                    active
+                    direction="desc"
+                  >
+                    Date
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+              <TableCell>
+                Status
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((order) => (
+              <TableRow
+                hover
+                key={order.id}
+              >
+                <TableCell>
+                  {order.ref}
+                </TableCell>
+                <TableCell>
+                  {order.customer.name}
+                </TableCell>
+                <TableCell>
+                  {format(order.createdAt, 'dd/MM/yyyy')}
+                </TableCell>
+                <TableCell>
+                  <SeverityPill
+                    color={(order.status === 'approved' && 'success')
+                    || (order.status === 'disapproved' && 'error')
+                    || 'warning'}
+                  >
+                    {order.status}
+                  </SeverityPill>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </PerfectScrollbar>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        p: 2
+      }}
+    >
+      <Button
+        color="primary"
+        endIcon={<ArrowRightIcon fontSize="small" />}
+        size="small"
+        variant="text"
+      >
+        View all
+      </Button>
+    </Box>
+  </Card>
+);
