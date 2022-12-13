e3332de80608f4441a88fc7a4635f0c4bbd1b1ac
nisayilmaz <nisayilmaz221@gmail.com>
2022-12-13 19:57:29 +0300

    erasmus application crud, outgoingstudenterasmus changed to outgoingstudent

--- backend/src/main/java/com/bilkent/erasmus/models/applicationModels/InitialApplicationModels/ApplicationErasmus.java
+++ backend/src/main/java/com/bilkent/erasmus/models/applicationModels/InitialApplicationModels/ApplicationErasmus.java
@@ -1,46 +1,29 @@
 package com.bilkent.erasmus.models.applicationModels.InitialApplicationModels;
 
 import com.bilkent.erasmus.models.universityModels.PartnerUniversityErasmus;
-import com.bilkent.erasmus.models.userModels.StudentModels.OutGoingStudentErasmus;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Entity
 //@DiscriminatorValue("erasmus")
-public class ApplicationErasmus extends Application implements Comparable<ApplicationErasmus> {
+public class ApplicationErasmus extends Application {
 
-    @ManyToOne
-    private OutGoingStudentErasmus student;
 
     @ManyToOne
     private PartnerUniversityErasmus assignedUniversity;
 
     @ManyToMany
     @JoinTable(
-            name = "app_uni",
+            name = "erasmusApplicationDetail",
             joinColumns = @JoinColumn(
-                    name = "app_id", referencedColumnName = "id"
+                    name = "applicationId", referencedColumnName = "id"
             ),
             inverseJoinColumns = @JoinColumn(
-                    name = "uni_id", referencedColumnName = "id"
+                    name = "universityId", referencedColumnName = "id"
             )
     )
     private List<PartnerUniversityErasmus> schools;
-
-    @Override
-    public int compareTo(ApplicationErasmus o) {
-        if(student.getErasmusPoint().equals(o.student.getErasmusPoint())) {
-            return 0;
-        }
-        else if (student.getErasmusPoint() > o.student.getErasmusPoint()){
-            return 1;
-        }
-        else {
-            return -1;
-        }
-    }
 }
--- backend/src/main/java/com/bilkent/erasmus/repositories/applicationRepositories/ApplicationErasmusRepository.java
+++ backend/src/main/java/com/bilkent/erasmus/repositories/applicationRepositories/ApplicationErasmusRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ApplicationErasmusRepository extends JpaRepository<ApplicationErasmus, Integer> {
 
     List<ApplicationErasmus> findAllByStatus(Status status);
+    List<ApplicationErasmus> findAllByStatusOrderByStudent_ErasmusPointDesc(Status status);
+    List<ApplicationErasmus> findAllByStatusOrderByStudent_ErasmusPointAsc(Status status);
+    Optional<ApplicationErasmus> findByStudent_StarsId(String starsId);
+
+
 }
