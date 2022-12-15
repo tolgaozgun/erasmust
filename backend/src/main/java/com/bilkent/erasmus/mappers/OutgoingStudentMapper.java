a2ad629645ca84ff0728c2e96e292fce507b9273
nisayilmaz <nisayilmaz221@gmail.com>
2022-12-16 00:02:40 +0300

    admin add student, list students

---
+++ backend/src/main/java/com/bilkent/erasmus/mappers/OutgoingStudentMapper.java
@@ -0,0 +1,10 @@
+package com.bilkent.erasmus.mappers;
+
+import com.bilkent.erasmus.models.userModels.StudentModels.OutGoingStudent;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface OutgoingStudentMapper {
+
+
+}
