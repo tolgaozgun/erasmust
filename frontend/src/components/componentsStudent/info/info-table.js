0860aa86b1d9a7eb5d7e912b71dfc687080f3556
Tolga Özgün <tolgaozgunn@gmail.com>
2022-12-16 21:18:21 +0300

    Added better View pages for Erasmus and Preapproval application

--- frontend/src/components/componentsStudent/info/info-table.js
+++ frontend/src/components/componentsStudent/info/info-table.js
@@ -9,24 +9,37 @@ import {
 } from '@mui/material';
 import React from 'react';
 import Paper from "@mui/material/Paper";
+import {styled} from "@mui/material/styles";
 
 export const InfoTable = (props) => {
+
+    const StyledTableRow = styled(TableRow)(({theme}) => ({
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+            border: 0,
+        },
+    }));
+
 
     console.log(props.items)
     return (
         <TableContainer component={Paper}>
             <Table aria-label="simple table">
-                <TableBody>
+                <TableBody stripedRows>
                     {Object.keys(props.items).map(item => (
-                        <TableRow
+                        <StyledTableRow
                             key={item}
+                            color="green"
                             sx={{'&:last-child td, &:last-child th': {border: 0}}}
                         >
                             <TableCell component="th" scope="row">
                                 {item}
                             </TableCell>
                             <TableCell align="right">{props.items[item]}</TableCell>
-                        </TableRow>
+                        </StyledTableRow>
                     ))}
                 </TableBody>
             </Table>
