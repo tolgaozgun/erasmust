1edc02c276b4234fc43be169f4309a9ef539159e
ebuyukkonuklu <buyukkonukluemirhan@gmail.com>
2022-12-16 22:19:57 +0300

    routeConfig modified

--- frontend/src/routeConfig.tsx
+++ frontend/src/routeConfig.tsx
@@ -15,7 +15,6 @@ import Preapproval from "./pages/student/Preapproval/PreapprovalForm"
 import BeforeMobility from "./pages/student/LearningAgreement/BeforeMobility"
 import DuringMobility from "./pages/student/LearningAgreement/DuringMobility"
 import AfterMobility from "./pages/student/LearningAgreement/AfterMobility"
-import ViewBeforeMobility from "./pages/student/LearningAgreement/ViewBeforeMobility"
 import ErasmusApplicationForm from "./pages/student/ErasmusApplication/erasmusApplication"
 import CTForm from "./pages/student/courseTransferForm";
 import ViewPreapproval from "./pages/student/Preapproval/ViewPreapproval"
@@ -55,7 +54,8 @@ import ExchangeFaq from "./pages/student/exchangeFaq";
 import Submissions from "./pages/academic/submissions";
 
 
-export const routeItems: Array<IRoute> = [
+// Student
+export const routeItemsStudent: Array<IRoute> = [
     {
         path: '/registerStudent',
         title: "Student Register",
@@ -93,6 +93,235 @@ export const routeItems: Array<IRoute> = [
         exact: true,
     },
     {
+        path: '/exchangeFaq',
+        title: "Exchange FAQ",
+        content: <ExchangeFaq/>,
+        exact: true,
+    },
+
+    {
+        path: '/preapproval',
+        title: "Preapproval Form",
+        content: <Preapproval/>,
+        exact: true,
+    },
+    {
+        path: 'learningAgreement/beforeMobility',
+        title: "Learning Agreement Before Mobility",
+        content: <BeforeMobility/>,
+        exact: true,
+    },
+    {
+        path: 'learningAgreement/duringMobility',
+        title: "Learning Agreement During Mobility",
+        content: <DuringMobility/>,
+        exact: true,
+    },
+    {
+        path: 'learningAgreement/afterMobility',
+        title: "Learning Agreement After Mobility",
+        content: <AfterMobility/>,
+        exact: true,
+    },
+
+    {
+        path: '/ViewErasmusApplication',
+        title: "View Erasmus Application",
+        content: <ViewErasmusApplication/>,
+        exact: true,
+    },
+
+    {
+        path: '/preapprovals',
+        title: "Preapproval List",
+        content: <Preapprovals/>,
+        exact: true,
+    },
+
+    {
+        path: '*',
+        title: "Student Register",
+        content: <Error/>,
+        exact: false,
+    },
+    {
+        path: '/splash',
+        title: "Student Register",
+        content: <Splash/>,
+        exact: true,
+    },
+    {
+        path: '/',
+        title: "Student Register",
+        content: <Navigate to='/splash' />,
+        exact: true,
+    },
+
+    {
+        path: "/schoolList",
+        title: "School List",
+        content: <SchoolList/>,
+        exact: true,
+    },
+    {
+        path: '/viewPreapproval',
+        title: "View Preapproval",
+        content: <ViewPreapproval/>,
+        exact: true,
+    },
+    {
+        path: '/viewErasmusApplication',
+        title: "View Erasmus Application",
+        content: <ViewErasmusApplication/>,
+        exact: true,
+    },
+    {
+        path: '/courseReviewForms',
+        title: "Course Review Forms",
+        content: <CourseReviewForms/>,
+        exact: true,
+    },
+
+    {
+        path: '/viewPreapproval',
+        title: "View Preapproval Form",
+        content: <ViewPreapproval/>,
+        exact: true,
+    },
+    { 
+        path: '/applications',
+        title: "Applications",
+        content: <Applications/>,
+        exact: true,
+    },
+]
+
+
+// Admin
+export const routeItemsAdmin: Array<IRoute> = [
+    {
+        path: '/dashboardAdmin',
+        title: "Admin Dashboard",
+        content: <DashboardAdmin/>,
+        exact: true,
+    },
+    {
+        
+        path: '/studentListAdmin',
+        title: "Admin Account",
+        content: <StudentList/>,
+        exact: true,
+    },
+    {
+        
+        path: '/studentListAdmin/student/:id',
+        title: "Admin Account",
+        content: <StudentEdits/>,
+        exact: true,
+    },
+    {
+        
+        path: '/dashboardAdmin',
+        title: "Admin Dashboard",
+        content: <DashboardAdmin/>,
+        exact: true,
+    },
+    { 
+        path: '/accountAdmin',
+        title: "Admin Account",
+        content: <AccountAdmin/>,
+        exact: true,
+    },
+    {
+        path: '/studentlistAdmin',
+        title: "Admin Account",
+        content: <StudentList/>,
+        exact: true,
+    },
+
+    {
+        path: '/EditPreapproval',
+        title: "Edit Preapproval Form",
+        content: <EditPreapproval/>,
+        exact: true,
+    },
+
+    {
+        path: '/students',
+        title: "Students",
+        content: <Students/>,
+        exact: true,
+    },
+
+    {
+        path: '/preapprovals',
+        title: "Preapproval List",
+        content: <Preapprovals/>,
+        exact: true,
+    },
+
+    {
+        path: '*',
+        title: "Student Register",
+        content: <Error/>,
+        exact: false,
+    },
+    {
+        path: '/splash',
+        title: "Student Register",
+        content: <Splash/>,
+        exact: true,
+    },
+    {
+        path: '/',
+        title: "Student Register",
+        content: <Navigate to='/splash' />,
+        exact: true,
+    },
+    
+    {
+        path: "/schoolList",
+        title: "School List",
+        content: <SchoolList/>,
+        exact: true,
+    },
+    {
+        path: '/viewPreapproval',
+        title: "View Preapproval",
+        content: <ViewPreapproval/>,
+        exact: true,
+    },
+    {
+        path: '/viewErasmusApplication',
+        title: "View Erasmus Application",
+        content: <ViewErasmusApplication/>,
+        exact: true,
+    },
+    {
+        path: '/courseReviewForms',
+        title: "Course Review Forms",
+        content: <CourseReviewForms/>,
+        exact: true,
+    },
+
+    {
+        path: '/viewPreapproval',
+        title: "View Preapproval Form",
+        content: <ViewPreapproval/>,
+        exact: true,
+    },
+    { 
+        path: '/applications',
+        title: "Applications",
+        content: <Applications/>,
+        exact: true,
+    },
+]
+
+
+// Staff
+/*
+    {
         path: '/registerStaff',
         title: "Staff Register",
         content: <RegisterStaff/>,
@@ -129,84 +358,6 @@ export const routeItems: Array<IRoute> = [
         exact: true,
     },
     {
-        path: '/preapprovals',
-        title: "Preapproval List",
-        content: <Preapprovals/>,
-        exact: true,
-    },
-    {
-        path: '/preapproval',
-        title: "Preapproval Form",
-        content: <Preapproval/>,
-        exact: true,
-    },
-    {
-        path: 'learningAgreement/beforeMobility',
-        title: "Learning Agreement Before Mobility",
-        content: <BeforeMobility/>,
-        exact: true,
-    },
-    {
-        path: 'learningAgreement/ViewBeforeMobility',
-        title: "Learning Agreement View Before Mobility",
-        content: <ViewBeforeMobility/>,
-        exact: true,
-    },
-    {
-        path: 'learningAgreement/duringMobility',
-        title: "Learning Agreement During Mobility",
-        content: <DuringMobility/>,
-        exact: true,
-    },
-    {
-        path: 'learningAgreement/afterMobility',
-        title: "Learning Agreement After Mobility",
-        content: <AfterMobility/>,
-        exact: true,
-    },
-    {
-        path: '/viewPreapproval',
-        title: "View Preapproval Form",
-        content: <ViewPreapproval/>,
-        exact: true,
-    },
-    {
-        path: '/applications',
-        title: "Applications",
-        content: <Applications/>,
-        exact: true,
-    },
-    {
-        path: '/students',
-        title: "Students",
-        content: <Students/>,
-        exact: true,
-    },
-    {
-        path: '/erasmusApplication',
-        title: "Erasmus Application",
-        content: <ErasmusApplicationForm/>,
-        exact: true,
-    },
-    {
-        path: '/ViewErasmusApplication',
-        title: "View Erasmus Application",
-        content: <ViewErasmusApplication/>,
-        exact: true,
-    },
-    {
-        path: '/courseReviewForm',
-        title: "Course Review Form",
-        content: <CourseReviewForm/>,
-        exact: true,
-    },
-    {
-        path: '/ViewCourseReviewForm',
-        title: "View Review Form",
-        content: <ViewCourseReviewForm/>,
-        exact: true,
-    },
-    {
         path: '/StaffCourseReviewForm',
         title: "Staff Course Review Form",
         content: <StaffCourseReviewForm/>,
@@ -219,9 +370,36 @@ export const routeItems: Array<IRoute> = [
         exact: true,
     },
     {
-        path: '/EditPreapproval',
-        title: "Edit Preapproval Form",
-        content: <EditPreapproval/>,
+        path: "/submissions",
+        title: "Submissions",
+        content: <Submissions/>,
+        exact: true,
+    },
+
+    {
+        path: '/erasmusApplication',
+        title: "Erasmus Application",
+        content: <ErasmusApplicationForm/>,
+        exact: true,
+    },
+
+    {
+        path: '/courseReviewForm',
+        title: "Course Review Form",
+        content: <CourseReviewForm/>,
+        exact: true,
+    },
+    {
+        path: '/ViewCourseReviewForm',
+        title: "View Review Form",
+        content: <ViewCourseReviewForm/>,
+        exact: true,
+    },
+
+    {
+        path: '/ViewErasmusApplication',
+        title: "View Erasmus Application",
+        content: <ViewErasmusApplication/>,
         exact: true,
     },
     {
@@ -230,66 +408,7 @@ export const routeItems: Array<IRoute> = [
         content: <CTForm/>,
         exact: true,
     },
-    {
-        path: '/dashboardAdmin',
-        title: "Admin Dashboard",
-        content: <DashboardAdmin/>,
-        exact: true,
-    },
-    {
-        path: "/schoolList",
-        title: "School List",
-        content: <SchoolList/>,
-        exact: true,
-    },
-    {
-        path: '/studentListAdmin',
-        title: "Admin Account",
-        content: <StudentList/>,
-        exact: true,
-    },
-    {
-        path: '/studentListAdmin/student/:id',
-        title: "Admin Account",
-        content: <StudentEdits/>,
-        exact: true,
-    },
-    {
-        path: '/dashboardAdmin',
-        title: "Admin Dashboard",
-        content: <DashboardAdmin/>,
-        exact: true,
-    },
-    {
-        path: '/accountAdmin',
-        title: "Admin Account",
-        content: <AccountAdmin/>,
-        exact: true,
-    },
-    {
-        path: '/viewPreapproval',
-        title: "View Preapproval",
-        content: <ViewPreapproval/>,
-        exact: true,
-    },
-    {
-        path: '/viewErasmusApplication',
-        title: "View Erasmus Application",
-        content: <ViewErasmusApplication/>,
-        exact: true,
-    },
-    {
-        path: '/studentlistAdmin',
-        title: "Admin Account",
-        content: <StudentList/>,
-        exact: true,
-    },
-    {
-        path: '/courseReviewForms',
-        title: "Course Review Forms",
-        content: <CourseReviewForms/>,
-        exact: true,
-    },
+
     {
         path: '*',
         title: "Student Register",
@@ -308,17 +427,44 @@ export const routeItems: Array<IRoute> = [
         content: <Navigate to='/splash' />,
         exact: true,
     },
+
     {
-        path: '/exchangeFaq',
-        title: "Exchange FAQ",
-        content: <ExchangeFaq/>,
+        path: "/schoolList",
+        title: "School List",
+        content: <SchoolList/>,
         exact: true,
     },
     {
-        path: "/submissions",
-        title: "Submissions",
-        content: <Submissions/>,
+        path: '/viewPreapproval',
+        title: "View Preapproval",
+        content: <ViewPreapproval/>,
         exact: true,
-    }
-]
+    },
+    {
+        path: '/viewErasmusApplication',
+        title: "View Erasmus Application",
+        content: <ViewErasmusApplication/>,
+        exact: true,
+    },
+    {
+        path: '/courseReviewForms',
+        title: "Course Review Forms",
+        content: <CourseReviewForms/>,
+        exact: true,
+    },
+
+    {
+        path: '/viewPreapproval',
+        title: "View Preapproval Form",
+        content: <ViewPreapproval/>,
+        exact: true,
+    },
+    { 
+        path: '/applications',
+        title: "Applications",
+        content: <Applications/>,
+        exact: true,
+    },
+*/
+
 
