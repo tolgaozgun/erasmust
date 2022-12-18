0fa71e91a32a996eef79a0cff6502eb6f2d37003
Yeahya35 <yahyaeren35@hotmail.com>
2022-12-18 20:28:30 +0300

    patch

---
+++ backend/src/main/java/com/bilkent/erasmus/dtos/LoginStudentResponse.java
@@ -0,0 +1,14 @@
+package com.bilkent.erasmus.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginStudentResponse {
+}
