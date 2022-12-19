13aad514bf66f49ff1507ea317063d88b2c52b1b
Yeahya35 <yahyaeren35@hotmail.com>
2022-12-20 00:10:08 +0300

    cors patch

--- backend/src/main/java/com/bilkent/erasmus/security/MyCORSFilter.java
+++ backend/src/main/java/com/bilkent/erasmus/security/MyCORSFilter.java
@@ -23,6 +23,7 @@ public class MyCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
+        //response.setHeader("access-control-expose-headers", "*");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
 
         chain.doFilter(req, res);
