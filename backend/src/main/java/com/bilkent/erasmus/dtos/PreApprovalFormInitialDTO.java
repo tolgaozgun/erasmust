3f9d9281c56cef545c50ea01c9c62c4539cc8f28
Yeahya35 <yahyaeren35@hotmail.com>
2022-12-20 13:16:13 +0300

    feth initial datas

---
+++ backend/src/main/java/com/bilkent/erasmus/dtos/PreApprovalFormInitialDTO.java
@@ -0,0 +1,15 @@
+package com.bilkent.erasmus.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PreApprovalFormInitialDTO {
+
+    private String userName;
+}
